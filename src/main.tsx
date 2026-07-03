53594f442ee5815e6624c077bb83c33e551806f7
Jileyitayo <jileyitayo@gmail.com>
2026-07-03 01:38:44 -0500

    refactor: updated css references

--- src/main.tsx
+++ src/main.tsx
@@ -5,7 +5,7 @@ import { QueryClientProvider } from '@tanstack/react-query';
 import { queryClient } from './lib/queryClient';
 import { App } from './App';
 import './index.css';
-import './readymag.css';
+import './canvas.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
